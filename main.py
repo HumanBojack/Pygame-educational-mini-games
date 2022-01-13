eae181c5783e9763122084e5be44a45703d42d88
ThanhLan <manondoublier@yahoo.fr>
2022-01-13 14:28:48 +0100

    create game loop

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ pygame.init()
 #generate game display
 pygame.display.set_caption("Simplon Python Quest")
 screen = pygame.display.set_mode((1600,900))
+
+
+running = True
+#game loop
+
+while running:
+    for event in pygame.event.get():
+        # Game loop end => player quit the game
+        if event.type == pygame.QUIT:
+            running = False
