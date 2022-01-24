9624905624d137f0584fa05c6c4ddfb7566d09db
ThanhLan <manondoublier@yahoo.fr>
2022-01-24 13:52:47 +0100

    collisions walls

--- overworld/scripts/game.py
+++ overworld/scripts/game.py
@@ -23,6 +23,13 @@ class Game:
         # dessiner le groupe de calques
         self.group = pyscroll.PyscrollGroup(map_layer = map_layer, default_layer = 1)
         self.group.add(self.character)
+
+       #define a list to stock collision rectangles
+        self.walls = []
+
+        for obj in self.tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
     
     def handle_input(self):
         """ This function asociate caps of key to character moves """
@@ -37,14 +44,15 @@ class Game:
         elif pressed[pygame.K_RIGHT]:
             self.character.move_right()
         
+    def update(self):
+        self.group.update()
 
+        #check collision
+        for sprite in self.group.sprites():
+            if sprite.feet.collidelist(self.walls) > -1:
+                sprite.move_back()
 
-        #définir une liste qui va stocker les rectangles de collision
-        self.walls = []
-
-        for obj in self.tmx_data.objects:
-            if obj.type == "collision":
-                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+        
 
     def run(self):
     # Boucle du jeu
@@ -53,8 +61,9 @@ class Game:
     
         running = True
         while running:
+            self.character.save_location()
             self.handle_input()
-            self.group.update()
+            self.update()
             self.group.draw(self.screen)
             pygame.display.flip()
 
@@ -66,10 +75,4 @@ class Game:
 
         pygame.quit()
 
-    def update(self):
-        self.group.update()
-
-        #vérification collision
-        # for sprite in self.group.sprites():
-            # if sprite.feet.collidelist(self.walls) > -1:
-                # sprite.move_back()
+ 
