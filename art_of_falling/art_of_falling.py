9441ebcb073a531eabc9d423a3ee2db0bf6220f9
HumanBojack <spychalar59@gmail.com>
2022-01-25 10:10:00 +0100

    displayed gamemode

--- art_of_falling/art_of_falling.py
+++ art_of_falling/art_of_falling.py
@@ -49,19 +49,24 @@ class ArtOfFalling:
     if self.gamemode == "which_is_good":
       position_x = (640 - self.wig_question.get_rect().w) / 2
       self.screen.blit(self.wig_question, (position_x, 420))
-    self.update_score()
-    self.update_health()
+    self.display_score()
+    self.display_health()
+    self.display_gamemode()
     pygame.display.flip()
 
-  def update_score(self):
+  def display_score(self):
     scoredraw = f"Score: {int(self.player.score * self.difficulty)} (x{self.difficulty})"
     scoredraw = self.hud_font.render(scoredraw, True, (192,255,255))
     self.screen.blit(scoredraw, (10, 462)) # (50, 420) (420, 30) (10, 462) (425, 10)
   
-  def update_health(self):
+  def display_health(self):
     health_display = f"{self.player.health} life remaining"
     health_display = self.hud_font.render(health_display, True, (0,0,0))
     self.screen.blit(health_display, (440,10))
+
+  def display_gamemode(self):
+    gamemode = self.hud_font.render(self.gamemode, True, (0,0,0))
+    self.screen.blit(gamemode, (10, 10))
 
   def menu(self):
     menu = Menu(self)
@@ -98,10 +103,8 @@ class ArtOfFalling:
 
   def wig_select_word(self):
     level = random.choice(self.wig_words)
-
     self.wig_words.remove(level)
     if not self.wig_words:
-      print("reloading")
       self.load_wig_words()
 
     question = level["question"]
@@ -110,7 +113,6 @@ class ArtOfFalling:
 
     random.shuffle(words)
     index = words.index(good_answer)
-
     return words, index, question
 
   def which_is_good(self):
@@ -124,12 +126,10 @@ class ArtOfFalling:
       self.rounds = 1
       self.gamemode = random.choice(["which_is_good", "which_is_python"])
     elif self.rounds % 5 == 0:
-      self.rounds = 0
       self.gamemode = ("which_is_good", "which_is_python")[self.gamemode == "which_is_good"]
 
   def execute_gamemode(self):
     self.gamemode_selector()
-
     self.rounds += 1
     if self.gamemode == "which_is_good":
       self.which_is_good()
