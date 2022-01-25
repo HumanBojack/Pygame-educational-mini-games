e9ce86479dcfc389cb401605ea3cad2263d61fff
HumanBojack <spychalar59@gmail.com>
2022-01-25 10:13:58 +0100

    cleaned code

--- art_of_falling/menu.py
+++ art_of_falling/menu.py
@@ -20,12 +20,9 @@ class Menu():
     nrml_color = ((128,128,128), (255,0,0))[self.selected_difficulty == 1.0]
     hard = self.font.render("Hard", True, hard_color)
     normal = self.font.render("Normal", True, nrml_color)
-    # normal_x = screensize[0]/2
-    # normal_y = screensize[1]/2
     screensize_ = self.game.screen.get_rect()
-    print(screensize_[2])
 
-    #place of menu
+    # Center the menu
     normal_police = normal.get_rect()
     hard_police = hard.get_rect()
 
