9a630bc62732e32aad8dad2e384e8026422dd57f
Thanh Lan <manondoublier@yahoo.fr>
2022-01-25 11:05:32 +0100

    update with door colision

--- overworld/scripts/game.py
+++ overworld/scripts/game.py
@@ -21,18 +21,22 @@ class Game:
         self.character= Character(30,40)
 
         # dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer = map_layer, default_layer = 1)
+        self.group = pyscroll.PyscrollGroup(map_layer = map_layer, default_layer = 3)
         self.group.add(self.character)
 
-       #define a list to stock collision rectangles
+       #define two lists to stock collision rectangles
         self.walls = []
+        self.doors = []
 
         for obj in self.tmx_data.objects:
             if obj.type == "collision":
                 self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+            elif obj.name =="art_of_fall":
+                self.doors.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
     
     def handle_input(self):
-        """ This function asociate caps of key to character moves """
+        """ This function associate caps of key to character moves """
         pressed = pygame.key.get_pressed()
 
         if pressed[pygame.K_UP]:
@@ -51,6 +55,9 @@ class Game:
         for sprite in self.group.sprites():
             if sprite.feet.collidelist(self.walls) > -1:
                 sprite.move_back()
+            elif sprite.feet.collidelist(self.doors) > -1:
+                print("launch game")
+
 
         
 
